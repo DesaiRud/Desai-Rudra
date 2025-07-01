7ff1eb7049546aecf841d25b5b998de34df1ccce
DesaiRud <151226749+DesaiRud@users.noreply.github.com>
2025-07-01 21:36:00 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -120,10 +120,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_URL='/images/'
 
-STATICFILES_DIRS=[
-  os.path.join(BASE_DIR, 'static')  
+import os
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
 ]
+
+# For production (like Render)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
